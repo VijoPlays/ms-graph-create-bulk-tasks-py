155ddbf7b55bba4fa2a8bf7fe9e9f4e6a4e96db8
Alexander "Vijo" Ott <blfasan@yahoo.de>
2023-03-04 09:26:21 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,2 @@
 # TODO: Implement to-do task bulk creation: https://learn.microsoft.com/en-us/graph/api/resources/todo-overview?view=graph-rest-1.0
+#TODO: Also make variable for plans/lists and loop over them to create n lists with m tasks //focus un py for work
